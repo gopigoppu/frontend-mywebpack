73ac08d928574e1684e1352ed6d327575f41a52c
gopigoppu <gopi.learner93@gmail.com>
2018-02-27 23:54:57 +0530

    autoprefixer and sourcemaps support add

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
 
 var styleSRC = "./src/scss/style.scss";
 var styleDIST = "./dist/css";
@@ -9,12 +11,18 @@ var styleDIST = "./dist/css";
 
 gulp.task('style', function () {
     gulp.src(styleSRC)
+        .pipe(sourcemaps.init())
         .pipe(sass({
             errorLogToConsole: true,
             outputStyle: 'compressed'
         }))
         .on('error', console.error.bind(console))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
         .pipe(rename({ suffix: '.min' }))
+        .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest(styleDIST));
 
 });
